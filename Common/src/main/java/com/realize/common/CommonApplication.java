d0fb883805e514c5b509cac1887170b4e8eb925b
songwenjun <2492824114@qq.com>
2021-10-03 21:06:24 +0800

    继续优化

--- Common/src/main/java/com/realize/common/CommonApplication.java
+++ Common/src/main/java/com/realize/common/CommonApplication.java
@@ -1,4 +1,4 @@
-package com.realize.register_app;
+package com.realize.common;
 
 import android.app.Application;
 
@@ -6,14 +6,14 @@ import com.realize.routeeasy.utils.Router;
 
 /**
  * Created by SongWenjun
- * 2021/10/1
+ * 2021/10/3
  * ∩ _ ∩
  * (??ω??)っ一? ?? ???
  * っ 丿         ? ????
  * 乚― J               ???
  * This class is ...
  */
-public class MyApp extends Application {
+public class CommonApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();

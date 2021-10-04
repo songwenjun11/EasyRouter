3775c49673a47518db80e48de8d432f69279678b
songwenjun <2492824114@qq.com>
2021-10-04 13:12:42 +0800

    添加跳转拦截器

--- Common/src/main/java/com/realize/common/CommonApplication.java
+++ Common/src/main/java/com/realize/common/CommonApplication.java
@@ -17,6 +17,6 @@ public class CommonApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Router.init(this);
+//        Router.init(this);
     }
 }
--- app/src/main/java/com/realize/easyrouter/MainActivity.kt
+++ app/src/main/java/com/realize/easyrouter/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.realize.easyrouter
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.realize.routeeasy.interfaces.Interceptor
 import com.realize.routeeasy.utils.Router
 import com.realize.router_note.EAction
 
-@EAction(path = "main/activity")
+@EAction(path = "action/activity/test")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.tv_button).setOnClickListener {
-            Router.getInstance().startAction("login/activity")
+            Router.getInstance()
+                .startAction("action/activity/test?asb=asdasd", object : Interceptor {
+                    override fun interceptor(action: String?, bundle: Bundle?): Boolean {
+                        Log.e("test", bundle.toString())
+                        return true
+                    }
+                })
         }
     }
 }

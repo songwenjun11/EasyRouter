4afcb095aa28ca67d6e527bb2485610213565478
songwenjun <2492824114@qq.com>
2021-10-04 12:45:05 +0800

    添加跳转拦截器

---
+++ RouteEasy/src/main/java/com/realize/routeeasy/interfaces/Interceptor.java
@@ -0,0 +1,16 @@
+package com.realize.routeeasy.interfaces;
+
+import android.os.Bundle;
+
+/**
+ * Created by SongWenjun
+ * 2021/10/4
+ * ∩ _ ∩
+ * (??ω??)っ一? ?? ???
+ * っ 丿         ? ????
+ * 乚― J               ???
+ * This class is ...
+ */
+public interface Interceptor {
+    boolean interceptor(String action,Bundle bundle);
+}
